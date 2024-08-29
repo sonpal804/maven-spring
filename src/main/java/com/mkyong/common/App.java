4f7cc35fd28f478388d0ad1d775304761795a808
jadon <sonpal.jadon@s-force.org>
2024-08-29 10:38:32 +0530

    'refactoring'

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -13,8 +13,7 @@ public class App
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
     	 
-
-
+         
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
         
